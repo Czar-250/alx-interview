562eb595d602dfef95560610a3c02d6bd5e37d84
Czar-250 <104949487+Czar-250@users.noreply.github.com>
2023-04-13 21:48:03 +0200

    Permitting Files

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -12,5 +12,4 @@ for i in range(30):
         random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
         random.randint(1, 1024)
     ))
-    sys.stdout.flush()
-    
+    sys.stdout.flush() 
